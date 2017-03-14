7e107d808daf7f1b6ef6669b364ff36eb8ec1c97
netdex <netdex593@gmail.com>
2017-03-13 23:01:57 -0400

    Complete overhaul Optmization Now supports video

--- AutoHinafier/AutoHinafier.Designer.cs
+++ AutoHinafier/AutoHinafier.Designer.cs
@@ -29,54 +29,94 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.imageBox = new Emgu.CV.UI.ImageBox();
-            this.btnImport = new System.Windows.Forms.Button();
+            this.btnImportImage = new System.Windows.Forms.Button();
             this.chkAnime = new System.Windows.Forms.CheckBox();
+            this.btnImportVideo = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.imageBox = new Emgu.CV.UI.ImageBox();
+            this.lblActiveVideo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnImportImage
+            // 
+            this.btnImportImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImportImage.Location = new System.Drawing.Point(693, 446);
+            this.btnImportImage.Margin = new System.Windows.Forms.Padding(2);
+            this.btnImportImage.Name = "btnImportImage";
+            this.btnImportImage.Size = new System.Drawing.Size(116, 24);
+            this.btnImportImage.TabIndex = 3;
+            this.btnImportImage.Text = "Import Image";
+            this.btnImportImage.UseVisualStyleBackColor = true;
+            this.btnImportImage.Click += new System.EventHandler(this.btnImportImage_Click);
+            // 
+            // chkAnime
+            // 
+            this.chkAnime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkAnime.AutoSize = true;
+            this.chkAnime.Location = new System.Drawing.Point(9, 450);
+            this.chkAnime.Margin = new System.Windows.Forms.Padding(2);
+            this.chkAnime.Name = "chkAnime";
+            this.chkAnime.Size = new System.Drawing.Size(55, 17);
+            this.chkAnime.TabIndex = 4;
+            this.chkAnime.Text = "Anime";
+            this.chkAnime.UseVisualStyleBackColor = true;
+            // 
+            // btnImportVideo
+            // 
+            this.btnImportVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImportVideo.Location = new System.Drawing.Point(581, 447);
+            this.btnImportVideo.Name = "btnImportVideo";
+            this.btnImportVideo.Size = new System.Drawing.Size(107, 23);
+            this.btnImportVideo.TabIndex = 5;
+            this.btnImportVideo.Text = "Import Video";
+            this.btnImportVideo.UseVisualStyleBackColor = true;
+            this.btnImportVideo.Click += new System.EventHandler(this.btnImportVideo_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPlay.Location = new System.Drawing.Point(485, 447);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 6;
+            this.btnPlay.Text = "Play/Pause";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // imageBox
             // 
             this.imageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.imageBox.Location = new System.Drawing.Point(12, 12);
+            this.imageBox.Location = new System.Drawing.Point(9, 25);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(2);
             this.imageBox.Name = "imageBox";
-            this.imageBox.Size = new System.Drawing.Size(488, 433);
+            this.imageBox.Size = new System.Drawing.Size(800, 417);
             this.imageBox.TabIndex = 2;
             this.imageBox.TabStop = false;
             // 
-            // btnImport
+            // lblActiveVideo
             // 
-            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnImport.Location = new System.Drawing.Point(425, 451);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(75, 29);
-            this.btnImport.TabIndex = 3;
-            this.btnImport.Text = "Import";
-            this.btnImport.UseVisualStyleBackColor = true;
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
-            // 
-            // chkAnime
-            // 
-            this.chkAnime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.chkAnime.AutoSize = true;
-            this.chkAnime.Location = new System.Drawing.Point(12, 456);
-            this.chkAnime.Name = "chkAnime";
-            this.chkAnime.Size = new System.Drawing.Size(69, 21);
-            this.chkAnime.TabIndex = 4;
-            this.chkAnime.Text = "Anime";
-            this.chkAnime.UseVisualStyleBackColor = true;
-            this.chkAnime.CheckedChanged += new System.EventHandler(this.chkHuman_CheckedChanged);
+            this.lblActiveVideo.AutoSize = true;
+            this.lblActiveVideo.Location = new System.Drawing.Point(12, 9);
+            this.lblActiveVideo.Name = "lblActiveVideo";
+            this.lblActiveVideo.Size = new System.Drawing.Size(0, 13);
+            this.lblActiveVideo.TabIndex = 7;
             // 
             // AutoHinafier
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 486);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(818, 475);
+            this.Controls.Add(this.lblActiveVideo);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnImportVideo);
             this.Controls.Add(this.chkAnime);
-            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnImportImage);
             this.Controls.Add(this.imageBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AutoHinafier";
             this.Text = "AutoHinafier";
             this.Load += new System.EventHandler(this.AutoHinafier_Load);
@@ -89,7 +129,10 @@
         #endregion
 
         private Emgu.CV.UI.ImageBox imageBox;
-        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnImportImage;
         private System.Windows.Forms.CheckBox chkAnime;
+        private System.Windows.Forms.Button btnImportVideo;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblActiveVideo;
     }
 }
--- AutoHinafier/AutoHinafier.cs
+++ AutoHinafier/AutoHinafier.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
@@ -20,50 +24,167 @@ namespace AutoHinafier
             InitializeComponent();
         }
 
-        private CascadeClassifier _cascadeClassifier;
-        private Image<Bgra, Byte> _hina;
+        private Dictionary<FaceType, CascadeClassifier> CascadeClassifiers = new Dictionary<FaceType, CascadeClassifier>();
+        private Dictionary<FaceType, RectangleF> AdjustmentFactor = new Dictionary<FaceType, RectangleF>();
+
+        private Image<Bgra, byte> _hina;
+
+        private Capture VideoCapture;
+        private int ThreadWait;
+        private Stopwatch Time;
+        private bool VideoPlaying;
+        private long LastTick;
 
         private void AutoHinafier_Load(object sender, EventArgs e)
         {
-            _cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
-            _hina = new Image<Bgra, Byte>(Properties.Resources.Hinaface);
+            LoadResources();
         }
 
-        private void btnImport_Click(object sender, EventArgs e)
+        private void LoadResources()
+        {
+            CascadeClassifiers[FaceType.Anime] = new CascadeClassifier(Application.StartupPath + "/lbpcascade_animeface.xml");
+            CascadeClassifiers[FaceType.Human] = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            AdjustmentFactor[FaceType.Anime] = new RectangleF(1f / 5, 1f / 5, 3f / 5, 3f / 5);
+            AdjustmentFactor[FaceType.Human] = new RectangleF(1f / 8, 1f / 8, 6f / 8, 6f / 8);
+            _hina = new Image<Bgra, byte>(Properties.Resources.Hinaface);
+        }
+
+        private void btnImportImage_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog { Filter = "Image|*.jpg;*.png;*.bmp;*.gif" };
             var result = ofd.ShowDialog();
             if (result == DialogResult.OK)
             {
-                var imageFrame = new Image<Bgra, Byte>(ofd.FileName);
-                var grayframe = imageFrame.Convert<Gray, byte>();
-                var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty);
-                
-                foreach (var face in faces)
-                {
-                    Rectangle adjusted;
-                    if (chkAnime.Checked)
-                        adjusted = new Rectangle(face.X + face.Width / 5, face.Y + face.Height / 5, face.Width * 3 / 5, face.Width * 3 / 5);
-                    else
-                        adjusted = new Rectangle(face.X + face.Width / 8, face.Y + face.Height / 8, face.Width * 6 / 8, face.Width * 6 / 8);
-                    imageFrame.ROI = adjusted;
-                    var overlay = _hina.Resize(adjusted.Width, adjusted.Height, Inter.Area, false);
-                    var k = adjusted.Width / 8;
-                    if (k % 2 == 0) k++;
-                    imageFrame._SmoothGaussian(k);
-                    imageFrame._And(overlay);
-                    imageFrame.ROI = Rectangle.Empty;
-                }
+                var imageFrame = new Image<Bgr, byte>(ofd.FileName);
+                DoHina(imageFrame, 3, chkAnime.Checked ? FaceType.Anime : FaceType.Human);
                 imageBox.Image = imageFrame;
             }
         }
-
-        private void chkHuman_CheckedChanged(object sender, EventArgs e)
+        private void btnImportVideo_Click(object sender, EventArgs e)
         {
-            if(chkAnime.Checked)
-                _cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/lbpcascade_animeface.xml");
-            else
-                _cascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Video|*.wmv;*.mp4;*.avi;*.mkv" };
+            var result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                VideoPlaying = false;
+                VideoCapture = new Capture(ofd.FileName);
+                lblActiveVideo.Text = "Active Video: " + ofd.FileName;
+                Time = new Stopwatch();
+            }
         }
+
+        private void DoHina(Image<Bgr, byte> source, int sensitivity, FaceType type)
+        {
+            CascadeClassifier classifier;
+            if (CascadeClassifiers.ContainsKey(type))
+                classifier = CascadeClassifiers[type];
+            else return;
+
+            var adjustment = AdjustmentFactor[type];
+            var faces = classifier.DetectMultiScale(source, 1.1, sensitivity, Size.Empty);
+            var bitm = source.Bitmap;
+            Graphics g = Graphics.FromImage(bitm);
+            g.InterpolationMode = InterpolationMode.Low;
+            g.CompositingQuality = CompositingQuality.HighSpeed;
+            g.SmoothingMode = SmoothingMode.HighSpeed;
+            g.TextRenderingHint = TextRenderingHint.SystemDefault;
+            g.PixelOffsetMode = PixelOffsetMode.HighSpeed;
+
+            foreach (var face in faces)
+            {
+                Rectangle adjusted = new Rectangle(
+                    (int)(face.X + face.Width * adjustment.X), (int)(face.Y + face.Height * adjustment.Y),
+                    (int)(face.Width * adjustment.Width), (int)(face.Height * adjustment.Height));
+                source.ROI = adjusted;
+                var overlay = _hina.Resize(adjusted.Width, adjusted.Height, Inter.Area, false);
+
+                var k = adjusted.Width / 8;
+                if (k % 2 == 0) k++;
+                source._SmoothGaussian(k);
+
+                g.DrawImageUnscaled(overlay.Bitmap, source.ROI.X, source.ROI.Y);
+                overlay.Dispose();
+            }
+            g.Dispose();
+            source.ROI = Rectangle.Empty;
+        }
+
+        private Image<Bgr, byte> ActiveFrame;
+        private void VideoTick()
+        {
+            var mat = VideoCapture.QueryFrame();
+            
+            if (mat == null)
+            {
+                VideoCapture = null;
+                VideoPlaying = false;
+                return;
+            }
+            string[] debug =
+            {
+                $"Time: {VideoCapture.GetCaptureProperty(CapProp.PosMsec)}",
+                $"Frame: {VideoCapture.GetCaptureProperty(CapProp.PosFrames)}",
+                $"Delay: {ThreadWait}"
+            };
+
+            ThreadWait = (int)(1000 / VideoCapture.GetCaptureProperty(CapProp.Fps));
+            if (ActiveFrame != null)
+                ActiveFrame.Dispose();
+            var img = mat.ToImage<Bgr, byte>();
+            ActiveFrame = img.Resize(500, 500, Inter.Nearest, true);
+            img.Dispose();
+            mat.Dispose();
+
+            DoHina(ActiveFrame, 3, chkAnime.Checked ? FaceType.Anime : FaceType.Human);
+            for (int i = 0; i < debug.Length; i++)
+            {
+                CvInvoke.PutText(
+                ActiveFrame,
+                debug[i],
+                new Point(10, 20 + i * 10),
+                FontFace.HersheyPlain,
+                0.75,
+                new Bgr(0, 255, 0).MCvScalar);
+            }
+
+            imageBox.Image = ActiveFrame;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (VideoPlaying)
+            {
+                VideoPlaying = false;
+                Time.Stop();
+            }
+            else
+            {
+                if (VideoCapture != null)
+                {
+                    ThreadWait = (int) (1000 / VideoCapture.GetCaptureProperty(CapProp.Fps));
+                    if(Time != null)
+                        Time.Start();
+                    VideoPlaying = true;
+                    LastTick = 0;
+                    new Thread(() =>
+                    {
+                        while (VideoPlaying)
+                        {
+                            VideoTick();
+                            var pass = Time.ElapsedMilliseconds - LastTick;
+                            LastTick = Time.ElapsedMilliseconds;
+                            Thread.Sleep((int) Math.Max(0, ThreadWait - pass));
+                        }
+                    }).Start();
+                }
+            }
+        }
+
+    }
+
+    enum FaceType
+    {
+        Human,
+        Anime
     }
 }
